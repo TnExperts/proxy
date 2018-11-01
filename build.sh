3dc6469625b39431017ff60a946b69b35b38e2c0
olafrv <olafrv@gmail.com>
2018-11-01 08:53:04 -0400

    Puerto 80

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 
 if [ "$1" == "destroy" ]
 then
-	docker image rm olafrv/proxy:lastest
+	docker image rm olafrv/proxy:latest
 else
 	docker pull nginx
-	docker build -t olafrv/proxy:lastest .
+	docker build -t olafrv/proxy:latest .
 fi
 	
